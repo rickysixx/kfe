ceb53d198bc3f50b7b277317d2de8f07bf052af8
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-01-26 10:27:40 +0100

    Adjusted code style of kfe.cc.

--- src/kfe.cc
+++ src/kfe.cc
@@ -8,7 +8,7 @@ using namespace llvm;
 
 int main(int argc, char* argv[])
 {
-    int res = 0;
+    int exitCode = 0;
     driver drv;
     /***********************************************************************/
     /********* Inizializzazione del target (local machine) *****************/
@@ -23,7 +23,8 @@ int main(int argc, char* argv[])
     drv.module->setTargetTriple(TargetTriple);
     std::string Error;
     auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
-    if (!Target) {
+    if (!Target)
+    {
         llvm::errs() << Error;
         return 1;
     }
@@ -32,8 +33,7 @@ int main(int argc, char* argv[])
     auto Features = "";
     TargetOptions opt;
     auto RM = llvm::Optional<llvm::Reloc::Model>();
-    auto TheTargetMachine =
-        Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+    auto TheTargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
     /************************* Configurazione del modulo *****************/
     drv.module->setDataLayout(TheTargetMachine->createDataLayout());
     drv.module->setTargetTriple(TargetTriple);
@@ -41,34 +41,44 @@ int main(int argc, char* argv[])
     /************* Fine set-up per creazione codice oggetto ****************/
     /***********************************************************************/
     int i = 1;
-    std::string Filename =
-        ""; // Il default è che il codice oggetto non viene generato
-    while (i < argc) {
+    std::string Filename = ""; // Il default è che il codice oggetto non viene generato
+    while (i < argc)
+    {
         if (argv[i] == std::string("-p"))
+        {
             drv.trace_parsing = true; // Abilita tracce debug nel parser
+        }
         else if (argv[i] == std::string("-s"))
+        {
             drv.trace_scanning = true; // Abilita tracce debug nello scanner
+        }
         else if (argv[i] == std::string("-v"))
+        {
             drv.ast_print = true; // Stampa una rapp. esterna dell'AST
+        }
         else if (argv[i] == std::string("-o"))
-            Filename = argv[++i] +
-                       (std::string) ".o"; // Crea codice oggetto nel file indicato
-        else if (!drv.parse(argv[i])) {    // Parsing e creazione dell'AST
-            drv.codegen();                 // Visita AST e generazione dell'IR (su stdout)
-            if (Filename != "") {
+        {
+            Filename = argv[++i] + (std::string) ".o"; // Crea codice oggetto nel file indicato
+        }
+        else if (!drv.parse(argv[i]))
+        {                  // Parsing e creazione dell'AST
+            drv.codegen(); // Visita AST e generazione dell'IR (su stdout)
+            if (Filename != "")
+            {
                 /*****************************************************************/
                 /******************** Generazione codice oggetto *****************/
                 /*****************************************************************/
                 std::error_code EC;
                 raw_fd_ostream dest(Filename, EC, sys::fs::OF_None);
-                if (EC) {
+                if (EC)
+                {
                     errs() << "Could not open file: " << EC.message();
                     return 1;
                 }
                 legacy::PassManager pass;
                 auto FileType = CGFT_ObjectFile;
-                if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr,
-                                                          FileType)) {
+                if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType))
+                {
                     errs() << "TheTargetMachine can't emit a file of this type";
                     return 1;
                 }
@@ -77,9 +87,14 @@ int main(int argc, char* argv[])
                 outs() << "Wrote " << Filename << "\n";
                 return 0;
             }
-        } else
-            res = 1;
+        }
+        else
+        {
+            exitCode = 1;
+        }
+
         i++;
-    };
-    return res;
+    }
+
+    return exitCode;
 }
